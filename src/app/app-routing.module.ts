c3c2e8e7296ef202b3309bc88429711ab05d573d
Julien <Julien.berthau@orange.com>
2018-10-05 15:30:25 +0200

    Angular exercice 1 : Auth part is working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import {PostListComponent} from './post-list/post-list.component';
 import {PostFormComponent} from './post-list/post-form/post-form.component';
 import {SignupComponent} from './auth/signup/signup.component';
 import {SigninComponent} from './auth/signin/signin.component';
+import {AuthGuardService} from './services/auth-guard.service';
 
 const routes: Routes = [
     {path: 'auth/signup', component: SignupComponent},
     {path: 'auth/signin', component: SigninComponent},
-    {path: 'posts', component: PostListComponent},
-    {path: 'posts/new', component: PostFormComponent},
+    {path: 'posts', canActivate: [AuthGuardService], component: PostListComponent},
+    {path: 'posts/new', canActivate: [AuthGuardService], component: PostFormComponent},
     {path: '', redirectTo: 'posts', pathMatch: 'full'},
     {path: '**', redirectTo: 'posts'}
 ];
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,46 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import * as firebase from 'firebase';
+
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+    constructor() {
+    }
+
+    signOutUser() {
+        firebase.auth().signOut();
+    }
+
+    signInUser(email: string, password: string) {
+        return new Promise(
+            (resolve, reject) => {
+                firebase.auth().signInWithEmailAndPassword(email, password).then(
+                    () => {
+                        resolve();
+                    },
+                    (error) => {
+                        reject(error);
+                    }
+                );
+            }
+        );
+    }
+
+    createNewUser(email: string, password: string) {
+        return new Promise(
+            (resolve, reject) => {
+                firebase.auth().createUserWithEmailAndPassword(email, password).then(
+                    () => {
+                        resolve();
+                    },
+                    (error) => {
+                        reject(error);
+                    }
+                );
+            }
+        );
+    }
 }
